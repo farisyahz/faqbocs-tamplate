eee9b1a4d7959f21e43f6c79f605d064497769ca
Faris Syahzani <farisyahzani6@gmail.com>
2023-04-18 16:42:12 +0700

    second commit

---
+++ src/components/AccordionItem.tsx
@@ -0,0 +1,21 @@
+import {Collapse} from "react-collapse"
+import {useState} from "react"
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+export default function AccordionItem({question, answer}:any){
+  const [open, setOpen] = useState(false);
+  return(
+    <div className="mt-3">
+      <div className="rounded-3xl bg-gray-100 shadow-md">
+        <div className={`px-5 py-3 flex justify-between items-center align-middle cursor-pointer gap-3`} onClick={() => setOpen(!open)}>
+          <p className="font-poppins font-semibold">{question}</p>
+          {open ? <AiOutlineMinus className="w-5"/> : <AiOutlinePlus className="w-5"/>}
+        </div>
+        <Collapse isOpened={open}>
+          <div className="font-poppins pb-3 px-5 transition duration-300">{answer}</div>
+        </Collapse>
+      </div>
+      
+    </div>
+  )
+}
---
+++ src/components/Faqbox.tsx
@@ -0,0 +1,74 @@
+import Image from "next/image"
+import logo from "../assets/faqbocs-logo.png"
+import {BiSearch} from 'react-icons/bi'
+import {useState} from 'react'
+import AccordionItem from "./AccordionItem"
+import PopupQuestion from "./PopupQuestion"
+
+export default function(){
+  const data = [
+    {
+      id:1,
+      q:"Siapa nama saya?",
+      a: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus sit qui beatae obcaecati laborum fugit itaque atque illo quaerat labore."
+    },
+    {
+      id:2,
+      q:"Apa hobi saya?",
+      a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus sit qui beatae obcaecati laborum fugit itaque atque illo quaerat labore."
+    },
+    {
+      id:3,
+      q:"Apa pelajaran kesukaan saya ?",
+      a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus sit qui beatae obcaecati laborum fugit itaque atque illo quaerat labore."
+    },
+    {
+      id:4,
+      q:"Apakah saya sudah punya pacar ?",
+      a:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus sit qui beatae obcaecati laborum fugit itaque atque illo quaerat labore."
+    }
+  ]
+  const [search, setSearch] = useState("");
+  const [dataSearched, setDataSearched] = useState<{id: number, q: string, a: string}[]>(data);
+  const [pop, setPop] = useState(false);
+
+  const handleSearch = (e:any) => {
+    setSearch(e.target.value);
+    setDataSearched(data.filter((faq)=>faq.q.toLowerCase().includes(search.toLowerCase())))
+  }
+
+
+  return(
+    <>
+      {pop && <PopupQuestion setPop={setPop}/>}
+      <div className="flex flex-col min-h-[100vh] align-middle sm:max-w-md justify-between py-16 px-10 mx-auto">
+        <div className="mx-auto flex flex-col items-center">
+          <div className="bg-[#f4f4f4] w-36 h-36 shadow-md rounded-full flex">
+            <Image
+              src={logo}
+              width={100}
+              alt=""
+              className="m-auto"
+            />
+          </div>
+          <h1 className="text-2xl font-bold mt-5">Faqbocs.com</h1>
+          <div className={`bg-gray-100  transitions duration-300  text-sm rounded-full font-poppins px-5 py-3  mt-3  max-w-md  flex justify-between gap-3 items-center w-full`}>
+            <BiSearch className="text-xl"/>
+            <input id="search" type="text" className=" w-full transitions duration-300  outline-none bg-transparent " placeholder="Search..."
+            autoComplete='off' 
+            value={search}
+            onChange={handleSearch}/>
+          </div>
+          
+          <button className="bg-slate-900 text-slate-50 rounded-full w-full py-2 px-5 mt-3 font-semibold hover:bg-slate-700" onClick={() => setPop(true)}>Question?</button>
+          <section>
+            {
+              dataSearched.map((faq) => (<AccordionItem key={faq.id} question={faq.q} answer={faq.a}/>))
+            }
+        </section>
+        </div>
+      </div>
+    </>
+    
+  )
+}
