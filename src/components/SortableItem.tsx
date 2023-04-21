07658a4f435af7d88320589747ece0c4c111c878
Faris Syahzani <farisyahzani6@gmail.com>
2023-04-21 20:52:53 +0700

    blue-theme

--- src/components/SortableItem.tsx
+++ src/components/SortableItem.tsx
@@ -10,7 +10,7 @@ export default function SortableItem({id, q, a}:{id:number, q:string, a:string})
     listeners,
     setNodeRef,
     transform,
-    transition
+    transition,
   } = useSortable({id:id, data:{qna:{id:id, q:q, a:a}}})
 
   const style = {
@@ -21,16 +21,17 @@ export default function SortableItem({id, q, a}:{id:number, q:string, a:string})
   const [showAnswer, setShowAnswer] = useState(false);
 
   return(
-    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-      <div className="font-poppins w-full border-2 border-slate-900 bg-white rounded-lg flex mt-3">
+    <div ref={setNodeRef} style={style} className="touch-none" {...attributes} {...listeners}>
+      <div className="font-poppins w-full bg-white/50 backdrop-blur-md border-[1px] border-gray-200 rounded-lg flex mt-3">
         <span className="w-10 flex">
          <RxDragHandleDots2 className="text-2xl m-auto"/>
         </span>
         <div className="w-full p-3">
-          <div className="text-lg font-bold ">{q}</div>
-          <button className="py-2 px-3 m-1 bg-gray-100 font-semibold" 
+          <div className=" font-semibold mb-1">{q}</div>
+          {showAnswer && <p>{a}</p>}
+          <button className="py-1 px-2 mt-2 text-sm rounded-md bg-gray-100 font-semibold" 
           onClick={()=>setShowAnswer(!showAnswer)}>{showAnswer ? "Hide" : "Show"} answer</button>
-          {showAnswer && <p >{a}</p>}
+          
         </div>
         
       </div>
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import SortableItem from "@/components/SortableItem"
 import type { MouseEvent, KeyboardEvent } from 'react'
+import { HiPlus } from 'react-icons/hi';
 import { DndContext,
   closestCenter,
   useSensor,
@@ -13,11 +14,12 @@ import { PointerSensor } from "@dnd-kit/core";
 
 
 export default function Dashboard(){
+  
   const [data, setData] = useState<{id:number, q:string, a:string}[]>([
     {
       id:1,
       q:"Siapa nama saya?",
-      a:"Nama saya Faris Rafie Syahzani"
+      a:"Nama saya Faris Rafie Syahzani. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto reprehenderit porro magnam deleniti ad provident est autem totam? Enim, inventore."
     },
     {
       id:2,
@@ -87,24 +89,27 @@ export default function Dashboard(){
   const sensors = [useSensor(SmartPointerSensor)];
 
   return(
-    <div className="max-w-lg mx-auto mt-10">
-      <h1 className="font-poppins text-3xl font-semibold text-center">Dashboard</h1>
-      <DndContext
-        collisionDetection={closestCenter}
-        onDragEnd={handleDragEnd}
-        sensors={sensors}
-      >
-        <SortableContext
-          items={data}
-          
-          strategy={verticalListSortingStrategy}
+    <div className="w-[100vw] h-[100vh] bg-sky-300">
+      <div className="sm:max-w-lg w-[95%] mx-auto pt-10">
+        <h1 className="font-poppins text-3xl font-semibold text-center">Dashboard</h1>
+        <button className="bg-blue-600 mt-5 hover:bg-blue-500 flex justify-center items-center py-2 px-3 w-full font-poppins text-lg font-semibold gap-3 rounded-lg text-slate-50">Add Question<HiPlus className="text-2xl"/></button>
+        <DndContext
+          collisionDetection={closestCenter}
+          onDragEnd={handleDragEnd}
+          sensors={sensors}
         >
-          {data.map((item) => (<SortableItem key={item.id} {...item}/>))}
+          <SortableContext
+            items={data}
+            strategy={verticalListSortingStrategy}
+          >
+            {data.map((item) => (<SortableItem key={item.id} {...item}/>))}
 
-        </SortableContext>
+          </SortableContext>
 
-      </DndContext>
+        </DndContext>
 
+      </div>
     </div>
+    
   )
 }
