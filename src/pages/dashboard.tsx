3c3d0ba83600627195b42ee3e3fd80bb0526cfc4
Faris Syahzani <farisyahzani6@gmail.com>
2023-04-22 16:13:42 +0700

    add qna

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,4 @@
 import SortableItem from "@/components/SortableItem"
-import type { MouseEvent, KeyboardEvent } from 'react'
 import { HiPlus } from 'react-icons/hi';
 import { DndContext,
   closestCenter,
@@ -90,11 +89,29 @@ export default function Dashboard(){
 
   const sensors = [useSensor(SmartPointerSensor)];
 
+  const [newQuestion, setNewQuestion] = useState("")
+  const [newAnswer, setNewAnswer] = useState("")
+  const handleSumbit = () => {
+    setAddQuestion(!addQuestion)
+    setData([{id:(data.length+1), q:newQuestion, a:newAnswer}, ...data])
+  }
+
   return(
     <div className="w-[100vw] h-[100vh] bg-sky-300">
       <div className="sm:max-w-lg w-[95%] mx-auto pt-10">
         <h1 className="font-poppins text-3xl font-semibold text-center">Dashboard</h1>
-        
+
+        {!addQuestion && <button className="bg-blue-600 mt-5 hover:bg-blue-500 flex justify-center items-center py-2 px-3 w-full font-poppins font-semibold gap-1 rounded-lg text-slate-50" onClick={() => setAddQuestion(!addQuestion)}><HiPlus className="text-xl"/>Add Question</button>}
+        {addQuestion && (
+          <form onSubmit={handleSumbit} className="w-full p-5 flex font-poppins flex-col backdrop-blur-md bg-white/50 border-[1px] border-gray-200 rounded-lg gap-2 mt-3">
+            <label htmlFor="question">Question : </label>
+            <input type="text" name="question" id="question" className=" px-2 py-1 text-sm border-b-2 border-slate-400 bg-slate-50/70 focus:border-slate-800 outline-none w-full" autoFocus placeholder="Question..." value={newQuestion} onChange={(e)=>setNewQuestion(e.target.value)}/>
+            <label htmlFor="question">Answer : </label>
+            <input type="text" name="answer" id="answer" className=" px-2 py-1 text-sm border-b-2 border-slate-400 bg-slate-50/70 focus:border-slate-800 outline-none w-full" placeholder="Answer..."
+            value={newAnswer} onChange={(e)=>setNewAnswer(e.target.value)}/>
+            <button type="submit" className="bg-blue-600 text-sm text-slate-50 py-2 px-3 rounded-md font-semibold w-fit flex gap-2 mt-2" >Add <HiPlus className="text-lg"/></button>
+          </form>
+        )}
         
         <DndContext
           collisionDetection={closestCenter}
@@ -111,17 +128,7 @@ export default function Dashboard(){
 
         </DndContext>
 
-        {!addQuestion && <button className="bg-blue-600 mt-5 hover:bg-blue-500 flex justify-center items-center py-2 px-3 w-full font-poppins text-lg font-semibold gap-3 rounded-lg text-slate-50" onClick={() => setAddQuestion(!addQuestion)}>Add Question<HiPlus className="text-2xl" /></button>}
-        {addQuestion && (
-          <form className="w-full p-5 flex font-poppins flex-col backdrop-blur-md bg-white/50 border-[1px] border-gray-200 rounded-lg gap-2 mt-3">
-            <label htmlFor="question">Question : </label>
-            <input type="text" name="question" id="question" className=" px-2 py-1 text-sm border-b-2 border-slate-400 bg-slate-50/70 focus:border-slate-800 outline-none w-full" autoFocus placeholder="Question..." />
-            <label htmlFor="question">Answer : </label>
-            <input type="text" name="answer" id="answer" className=" px-2 py-1 text-sm border-b-2 border-slate-400 bg-slate-50/70 focus:border-slate-800 outline-none w-full" placeholder="Answer..."
-            />
-            <button type="submit" className="bg-blue-600 text-sm text-slate-50 py-2 px-3 rounded-md font-semibold w-fit flex gap-2 mt-2" onClick={() => setAddQuestion(!addQuestion)}>Add <HiPlus className="text-lg"/></button>
-          </form>
-        )}
+        
 
       </div>
     </div>
