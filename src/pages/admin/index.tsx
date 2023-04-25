106191f6a8430f973ed0bc73ed9dc058805e312b
Faris Syahzani <farisyahzani6@gmail.com>
2023-04-25 12:54:24 +0700

    scroll

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -79,7 +79,7 @@ export default function Dashboard(){
   }
 
   return(
-    <div className="min-w-[100vw] h-[100vh] overflow-y-auto overflow-x-hidden bg-orange-100 py-10">
+    <div className="min-w-[100vw] h-[100vh] overflow-x-hidden bg-orange-100 py-10">
       <div className="fixed top-20 left-40 w-40 h-40 rounded-full bg-orange-400 blur-3xl z-0"></div>
       <div className="fixed bottom-20 right-60 w-60 h-60 rounded-full bg-orange-400 blur-[100px] z-0"></div>
       <div className="sm:max-w-lg w-[95%] mx-auto relative z-10">
